959d1fb947a676f7eda538dbfdbfe431f5c90704
Madhumathy828 <92262264+Madhumathy828@users.noreply.github.com>
2021-10-10 20:22:12 +0530

    Add files via upload

---
+++ New.java
@@ -0,0 +1,11 @@
+
+public class New {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		C:\Users\Madhu Mathii\Downloads\chromedriver_win32.exe
+		System.setProperty("webdriver.chrome.driver", "D:\\Testing\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+	}
+
+}
